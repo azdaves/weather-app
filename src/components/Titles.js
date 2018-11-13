0dfc8edc08f39426874eac1753efde5b7a502b9d
Dave Schilling <ds@Daves-MacBook-Pro.local>
2018-11-13 11:53:52 -0700

    added bootstrap and changed children to SFC

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,13 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Titles extends Component {
-    
-    render() { 
-        return ( <div>
-            <h1 className="title-container__title">Weather Finder</h1>
-            <h3 className="title-container__subtitle">Find out the weather in your area.</h3>
-        </div> );
-    }
-}
+const Titles = () =>  (  <div>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <h3 className="title-container__subtitle">Find out the weather in your area.</h3>
+    </div>  );
+
  
 export default Titles;
